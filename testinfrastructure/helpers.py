58a89ed15d4dd0caa31d53657304ec09a06400d8
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-10-08 17:03:15 +0200

    sourced out a often used helper function to the infrastructure package

---
+++ testinfrastructure/helpers.py
@@ -0,0 +1,10 @@
+
+def pp(strng,env,comment=""):
+    pe(strng,env,comment)
+    
+
+def pe(strng,env,comment=""):
+    print('############################################')
+    print(comment+"\n"+strng+"=:")
+    print(eval(strng,env))
+    print('############################################')
