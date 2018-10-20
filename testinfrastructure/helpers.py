05f2e8316eebc642371dda7197610b72b9aa8bd3
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-10-20 12:04:34 +0200

    extended the reporting functions a bit

--- testinfrastructure/helpers.py
+++ testinfrastructure/helpers.py
@@ -1,9 +1,33 @@
-def pp(strng,env,comment=""):
-    pe(strng,env,comment)
+import inspect
+from string import Template
+def pp(var_name,env,comment=""):
+    thisframe=inspect.currentframe()
+    callerFrame=thisframe.f_back
+    callerName=callerFrame.f_code.co_name
+    value=eval(var_name,env)
+    text=Template("${n} = ${v}\n type: ${t}").substitute(
+            n=var_name,
+            v=value,
+            t=type(value))
+    printOut(callerName,text)
     
 
 def pe(strng,env,comment=""):
+    # this function is just nice for debugging
+    # it prints out the variable along with its 
+    # name.
+    thisframe=inspect.currentframe()
+    callerFrame=thisframe.f_back
+    callerName=callerFrame.f_code.co_name
+    value=eval(strng,env)
+    text=Template("${n} = ${v}\n type: ${t}").substitute(
+            n=strng,
+            v=value,
+            t=type(value))
+    printOut(callerName,text)
+
+def printOut(callerName,txt):
     print('############################################')
-    print(comment+"\n"+strng+"=:")
-    print(eval(strng,env))
+    print(Template("in function: ${cn}").substitute(cn=callerName))
+    print(txt)
     print('############################################')
