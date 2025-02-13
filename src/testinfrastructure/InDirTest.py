8ecdae259b3a74b7dc4205be564872040e5bdf05
Markus Müller <markus.mueller.1.g@googlemail.com>
2025-02-13 16:17:21 +0100

    added a new public instance method get_testDirPath to be overloaded by subclasses. In this way parameterized tests can create different directories for different parameters.

--- src/testinfrastructure/InDirTest.py
+++ src/testinfrastructure/InDirTest.py
@@ -5,34 +5,29 @@ import inspect
 import shutil
 import os
 
-
 class InDirTest(unittest.TestCase):
-    #def __init__(self, args):
-    #    st = inspect.stack()
-    #    # self.caller_dir_path = pathlib.Path(
-    #    #     os.path.dirname(os.path.abspath(inspect.getfile(st[-1].frame)))
-    #    # )
-    #    super().__init__(args)
-
+    
     def myDirPath():
         return pathlib.Path.cwd()
 
     def tmpDirPath():
         return __class__.myDirPath().joinpath("tmp")
 
-    def run(self, *args):
-        #testDirPath = __class__.tmpDirPath().joinpath(self.id())
+    def get_testDirPath(self):
         cls=self.__class__
-        testDirPath = cls.tmpDirPath().joinpath(f"{cls.__module__}.{cls.__name__}.{self._testMethodName}")
+        return cls.tmpDirPath().joinpath(f"{cls.__module__}.{cls.__name__}.{self._testMethodName}")
+
+    def run(self, *args):
+        cls=self.__class__
+        testDirPath=self.get_testDirPath()
         testDirName = testDirPath.as_posix()
+        print("testDirName",testDirName)
 
         self.oldDirName = os.getcwd()
         __class__.rootDir = pathlib.Path(self.oldDirName).parent.parent
         if testDirPath.exists():
             shutil.rmtree(testDirPath)
         testDirPath.mkdir(parents=True)
-        print("self.id()",self.id())
-        print("testDirName",testDirName)
 
         os.chdir(testDirName)
 
